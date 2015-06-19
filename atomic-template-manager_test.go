67a2531f7e114a8f3a05e182c7aa3dab8fff8813
Jerry Saravia <jsaravia@shapeup.com>
2015-06-19 18:15:27 -0400

    Adding tests

--- atomic-template-manager_test.go
+++ atomic-template-manager_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+//create template directory and sub directories
+func createDirs(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll(dir+"/templates/atoms/fonts", os.ModeDir|os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.MkdirAll(dir+"/templates/pages/front-page", os.ModeDir|os.ModePerm); err != nil {
+		t.Fatal(err)
+	}
+}
+func destroyDirs(t *testing.T) {
+	if err := os.RemoveAll("./templates"); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestMeetsInterface(t *testing.T) {
 	var _ Manager = New()
 }
@@ -22,5 +41,17 @@ func TestErrorOnExecutingTemplate(t *testing.T) {
 
 	if err == nil {
 		t.Fatal("Expected error but got none.")
+	}
+}
+
+func TestNoTemplatesAreFound(t *testing.T) {
+	createDirs(t)
+	defer destroyDirs(t)
+
+	var man Manager = New()
+
+	man.AddDirectory("./templates")
+	if len(man.Templates()) > 0 {
+		t.Fatal("There were templates even though the directories were empty.")
 	}
 }
